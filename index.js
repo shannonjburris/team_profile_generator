ff0b1d21f7d6eacfd050414fc212e786be8b87e6
Shannon <shannonjburris@gmail.com>
2021-07-20 22:03:08 -0400

    updated js to create html file on complete

--- index.js
+++ index.js
@@ -1,143 +1,125 @@
 //require classes
-const Manager       = require("./lib/Manager");
-const Engineer      = require("./lib/Engineer");
-const Intern        = require("./lib/Intern");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const generateHTML = require("./lib/htmlTemplate");
 //require our packages
-const inquirer      = require('inquirer');
-const fs            = require('fs');
-let team            = [];
+const inquirer = require('inquirer');
+const fs = require('fs');
+let team = [];
 
 
 
 init = () => {
     inquirer
-    .prompt([
+        .prompt([
+            {
+                type: 'list',
+                name: 'title',
+                message: 'What is the title of the employee?',
+                choices: ['Manager', 'Engineer', 'Intern']
+            },
+            {
+                type: 'input',
+                name: 'name',
+                message: 'What is their name?'
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: 'What is their ID number?'
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: 'What is their email?'
+            }
+
+        ]).then((answers) => {
+            switch (answers.title) {
+                case 'Manager':
+                    createManager(answers);
+                    break;
+                case 'Engineer':
+                    createEngineer(answers);
+                    break;
+                case 'Intern':
+                    createIntern(answers);
+                    break;
+                default:
+                    break;
+            }
+        });
+}
+
+createManager = (info) => {
+
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'officeNumber',
+            message: 'What is their office number?'
+        }
+    ]).then((officeNumber) => {
+        const manager = new Manager(info.name, info.id, info.email, officeNumber.officeNumber);
+        team.push(manager);
+        addEmployee();
+        //pass employee information to html
+    })
+}
+
+createEngineer = (info) => {
+    // create constructor
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'github',
+            message: 'What is their GitHub?'
+        }
+    ]).then((github) => {
+        const engineer = new Engineer(info.name, info.id, info.email, github.github);
+        team.push(engineer);
+        addEmployee();
+        //pass employee information to html
+    })
+}
+
+createIntern = (info) => {// create constructor
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'school',
+            message: 'What school do they attend?'
+        }
+    ]).then((school) => {
+        const intern = new Intern(info.name, info.id, info.email, school.school);
+        team.push(intern);
+        addEmployee();
+        //pass employee information to html
+    })
+}
+
+addEmployee = () => {
+    inquirer.prompt([
         {
             type: 'list',
-            name: 'title',
-            message: 'What is the title of the employee?',
-            choices: ['Manager', 'Engineer', 'Intern']
-        },
-        {
-            type: 'input',
-            name: 'name',
-            message: 'What is their name?'
-        },
-        {
-            type: 'input',
-            name: 'id',
-            message: 'What is their ID number?'
-        },
-        {
-            type: 'input',
-            name: 'email',
-            message: 'What is their email?'
+            name: 'addEmployee',
+            message: 'Would you like to add another employee?',
+            choices: ['Yes', 'No']
         }
-        
-    ]).then((answers) => {
-        switch (answers.title) {
-            case 'Manager':
-                createManager(answers);
-                break;
-            case 'Engineer':
-                createEngineer(answers);
-                break;
-            case 'Intern':
-                createIntern(answers);
-                break;
-            default:
-                break;
+    ]).then((answer) => {
+        console.log(answer.addEmployee);
+        if (answer.addEmployee === "Yes") {
+            init();
+        }
+        else {
+            // console.log(team);
+            const htmlPageContent = generateHTML(team);
+            fs.writeFile('index.html', htmlPageContent, (err) =>
+                err ? console.log(err) : console.log('Successfully created index.html!')
+            );
         }
     });
 }
 
-    createManager = (info) => {
-        inquirer.prompt([
-            {
-                type: 'input',
-                name: 'officeNumber',
-                message: 'What is their office number?'
-            }
-        ]).then((officeNumber) => {
-            team.push(
-                    {
-                     'title': info.title,
-                     'name': info.name,
-                     'id': info.id,
-                     'email': info.email,
-                     'officeNumber': officeNumber.officeNumber
-                }
-            )
-            addEmployee();
-            //pass employee information to html
-        })
-    }
-
-    createEngineer = (info) => {
-        inquirer.prompt([
-            {
-                type: 'input',
-                name: 'github',
-                message: 'What is their GitHub?'
-            }
-        ]).then((github) => {
-            team.push(
-                {
-                    'title': info.title,
-                    'name': info.name,
-                    'id': info.id,
-                    'email': info.email,
-                    'gitHub': gitHub.gitHub
-               }
-            )
-            addEmployee();
-            //pass employee information to html
-        })
-    }
-
-    createIntern = (info) => {
-        inquirer.prompt([
-            {
-                type: 'input',
-                name: 'school',
-                message: 'What school do they attend?'
-            }
-        ]).then((school) => {
-            addEmployee();
-            //pass employee information to html
-        })
-    }
-
-    addEmployee = () => {
-        inquirer.prompt([
-            {
-                type: 'list',
-                name: 'addEmployee',
-                message: 'Would you like to add another employee?',
-                choices: ['Yes', 'No']
-            }
-        ]).then((answer) => {
-            console.log(answer.addEmployee);
-            if (answer.addEmployee === "Yes") {
-                init();
-            }
-            else {
-                console.log(team);
-                const htmlPageContent = generateHTML(team);
-                fs.writeFile('index.html', htmlPageContent, (err) =>
-                      err ? console.log(err) : console.log('Successfully created index.html!')
-                    );
-            }
-        })
-    }
-
-    generateHTML = (team) => {
-        for (let index = 0; index < team.length; index++) {
-            if (team[index].title === 'Manager') {
-                //html markup for manager card
-            }
-            
-        }
-    }
-
-    init();
+init();
